d59e4116222ac8c02164df04e0d60299f58ece10
Mikel Severson <mkseve@gmail.com>
2016-11-19 19:50:36 -0800

    feat: add firebase to project

---
+++ public/scripts.js
@@ -0,0 +1,8 @@
+  var config = {
+    apiKey: "AIzaSyCaNY_SWcO7qmo1u0UcRYR58xsrGqEZNhQ",
+    authDomain: "learning-fcm-374d8.firebaseapp.com",
+    databaseURL: "https://learning-fcm-374d8.firebaseio.com/",
+    storageBucket: "learning-fcm-374d8.appspot.com",
+    messagingSenderId: "926182017010",
+  };
+  firebase.initializeApp(config);
