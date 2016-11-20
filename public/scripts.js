25e67e6b1a226b56a6e1f763c965c6bbeb9ba226
Mikel Severson <mkseve@gmail.com>
2016-11-20 17:28:42 -0600

    feat: create firebase skeleton project

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,61 @@
-  var config = {
-    apiKey: "AIzaSyCaNY_SWcO7qmo1u0UcRYR58xsrGqEZNhQ",
+var config = {
+    apiKey: "AIzaSyDQi3tYHYzcqjBHXkKyINSyJtrvAXN5LxY",
     authDomain: "learning-fcm-374d8.firebaseapp.com",
     databaseURL: "https://learning-fcm-374d8.firebaseio.com/",
     storageBucket: "learning-fcm-374d8.appspot.com",
     messagingSenderId: "926182017010",
   };
-  firebase.initializeApp(config);
+    
+firebase.initializeApp(config);
+console.log(firebase.app().currentToken);  // "[DEFAULT]"
+
+const messaging = firebase.messaging();
+
+messaging.requestPermission()
+.then(function() {
+  console.log('Notification permission granted.');
+  // TODO(developer): Retrieve an Instance ID token for use with FCM.
+  // ...
+})
+.catch(function(err) {
+  console.log('Unable to get permission to notify.', err);
+});
+
+
+// Get Instance ID token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+messaging.getToken()
+.then(function(currentToken) {
+  if (currentToken) {
+    sendTokenToServer(currentToken);
+    updateUIForPushEnabled(currentToken);
+  } else {
+    // Show permission request.
+    console.log('No Instance ID token available. Request permission to generate one.');
+    // Show permission UI.
+    updateUIForPushPermissionRequired();
+    setTokenSentToServer(false);
+  }
+})
+.catch(function(err) {
+  console.log('An error occurred while retrieving token. ', err);
+  showToken('Error retrieving Instance ID token. ', err);
+  setTokenSentToServer(false);
+});
+
+messaging.onTokenRefresh(function() {
+  messaging.getToken()
+  .then(function(refreshedToken) {
+    console.log('Token refreshed.');
+    // Indicate that the new Instance ID token has not yet been sent to the
+    // app server.
+    setTokenSentToServer(false);
+    // Send Instance ID token to app server.
+    sendTokenToServer(refreshedToken);
+    // ...
+  })
+  .catch(function(err) {
+    console.log('Unable to retrieve refreshed token ', err);
+    showToken('Unable to retrieve refreshed token ', err);
+  });
+});
